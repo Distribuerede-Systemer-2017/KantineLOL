2374db87c77d537678e04fa7cb2e8df841254e96
Anika Ahmed <Anika@anikas-air.hhk.dk>
2017-10-17 12:44:33 +0200

    Added getters and setters.

--- src/main/java/server/models/product.java
+++ src/main/java/server/models/product.java
@@ -1,4 +1,31 @@
 package server.models;
 
 public class product {
+
+
+    private int type;
+    private int id;
+    private String productName;
+    private String productPrice;
+
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
+    public String getProductPrice() {
+        return productPrice;
+    }
+
+    public void setProductPrice(String productPrice) {
+        this.productPrice = productPrice;
+    }
 }
