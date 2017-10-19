eac5f6b8eabea23fdd4d2306081a9849e1b426d4
max-emil kristensen <Max-Emil@max-emils-mbp.hhk.dk>
2017-10-19 20:00:21 +0200

    kryptering & logging

--- src/main/java/logic/controller/UserController.java
+++ src/main/java/logic/controller/UserController.java
@@ -1,15 +1,14 @@
 package logic.controller;
 
 import server.models.User;
-//import server.providers.UserProvider;
+import server.providers.UserProvider;
 
 import java.util.ArrayList;
 
 public class UserController {
     public UserController
 
-    public User validateUserCreation(String password,
-                                     String username) throws IllegalArgumentException {
+    public User validateUserCreation(String password, String username) throws IllegalArgumentException {
 
         User validatedUser = new User(password, username);
 
--- src/main/java/server/endpoints/ProductEndpoint.java
+++ src/main/java/server/endpoints/ProductEndpoint.java
@@ -9,16 +9,26 @@ import com.google.gson.Gson;
 import server.ServerImplDB.ImplDB;
 import server.models.Product;
 import server.providers.DrinkProvider;;
+import server.ServerImplDB.Kryptering;
 
 @Path("/drink")
 public class ProductEndpoint {
 
+    Kryptering kryptering = new Kryptering();
+
+
     @GET
     public Response getAllProducts() {
 
+        String json = new Gson().toJson(getAllProducts());
+        String krypteret = Kryptering.encryptdecrypt(json);
+
         ImplDB serverImplDB = new ImplDB();
         ArrayList<Product> allProducts = new DrinkProvider().getProducts();
-        return Response.status(200).type("application/json").entity(new Gson().toJson(allProducts)).build();
+        return Response.status(200)
+                .type("application/json")
+                .entity(krypteret)
+                .build();
 
 
 
