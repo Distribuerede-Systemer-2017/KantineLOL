72f905d637563007249407b39fe8028d83bdce32
Kristian <krso16ae@student.cbs.dk>
2017-10-19 14:49:12 +0200

    Vi har lavet createUser i user provider, og addUser i usercontroller, samt sat det sammen i UserEndpoint

--- src/main/java/logic/controller/UserController.java
+++ src/main/java/logic/controller/UserController.java
@@ -21,31 +21,20 @@ public class UserController {
 }
 /*
     public UserController
-
     public User validateUserCreation(String password,
                                      String username) throws IllegalArgumentException {
-
         User validatedUser = new User(password, username);
-
-
-
         String[] emailSplit;
         emailSplit = username.split("@");
         String username = emailSplit[0];
         String emailDomain = emailSplit[1];
-
         if (!emailDomain.contains("cbs.dk") && !emailDomain.contains("student.cbs.dk")
                 ) {
             System.out.print("e-mail er ikke korrekt\n");
             System.out.print(emailDomain);
             throw new IllegalArgumentException("domænet er forkert");
         }
-
-
         return validatedUser;
-
-
         //NOTE: Jeg havde glemt at man ikke skal tage højde for e-mail fordi vi bruger USERNAME og ikke Email
-
     }
 } */
--- src/main/java/server/providers/UserProvider.java
+++ src/main/java/server/providers/UserProvider.java
@@ -1,4 +1,33 @@
 package server.providers;
 
+import server.models.User;
+import server.ServerImplDB.ImplDB;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.sql.*;
+
 public class UserProvider {
+
+    private Connection connection;
+
+
+    public boolean createUser(User user) throws IllegalArgumentException {
+        try {
+
+            PreparedStatement createUser = connection
+                    .prepareStatement("INSERT INTO user (username, password) VALUES (?,?)");
+
+            createUser.setString(1, user.getUsername());
+            createUser.setString(2, user.getPassword());
+
+            int rowsAffected = createUser.executeUpdate();
+            if (rowsAffected == 1) {
+                return true;
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+
+        }
+        return false;
+    }
 }
